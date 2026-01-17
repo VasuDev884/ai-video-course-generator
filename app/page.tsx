cb20572ef3405b535dda4bcaf29a7e0248a0c5fb
Vasudev <115608959+VasuDev884@users.noreply.github.com>
2026-01-17 13:11:16 +0530

    new project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Button>hello</Button>
+    </div>
+  );
+}
